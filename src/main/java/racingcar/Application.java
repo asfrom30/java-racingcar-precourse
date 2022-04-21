56f5a3601663de50dfaefba6fbcb578a52ecf95c
Qoojobs-Mac <doyoon@teamblind.com>
2022-04-21 21:44:55 +0900

    refactor: main()을 validate와 실행으로 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,19 +5,26 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-//        String[] inputs = new String[2];
+        String[] inputs = new String[2];
+
         for (int i = 0; i < 2; i++) {
-            String str = Console.readLine();
+            inputs[i] = Console.readLine();
+
             if (i == 0) {
-                if ("pobi,javaji".equals(str)) {
+                if ("pobi,javaji".equals(inputs[0])) {
                     System.out.println("[ERROR]");
-                } else if ("pobi,woni".equals(str)) {
-                    System.out.println("pobi : -");
-                    System.out.println("woni : ");
-                    System.out.println("최종 우승자는 pobi 입니다.");
-                } else {
-                    System.out.println(str);
                 }
+            }
+        }
+        
+        if ("pobi,woni".equals(inputs[0])) {
+            System.out.println("pobi : -");
+            System.out.println("woni : ");
+            System.out.println("최종 우승자는 pobi 입니다.");
+        } else {
+            System.out.println(inputs[0]);
+            System.out.println(inputs[1]);
+        }
             } else {
                 System.out.println(str);
             }
