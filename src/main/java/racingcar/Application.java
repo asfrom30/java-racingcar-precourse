69a03caa2dbaca19e0319a908f03401489e354ef
Qoojobs-Mac <doyoon@teamblind.com>
2022-04-21 20:25:14 +0900

    feat: main()문은 두개의 입력을 받도록 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -5,13 +5,22 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        String str = Console.readLine();
-        if ("pobi,javaji".equals(str)) {
-            System.out.println("[ERROR]");
-        } else {
-            System.out.println("pobi : -");
-            System.out.println("woni : ");
-            System.out.println("최종 우승자는 pobi 입니다.");
+//        String[] inputs = new String[2];
+        for (int i = 0; i < 2; i++) {
+            String str = Console.readLine();
+            if (i == 0) {
+                if ("pobi,javaji".equals(str)) {
+                    System.out.println("[ERROR]");
+                } else if ("pobi,woni".equals(str)) {
+                    System.out.println("pobi : -");
+                    System.out.println("woni : ");
+                    System.out.println("최종 우승자는 pobi 입니다.");
+                } else {
+                    System.out.println(str);
+                }
+            } else {
+                System.out.println(str);
+            }
         }
     }
 }
