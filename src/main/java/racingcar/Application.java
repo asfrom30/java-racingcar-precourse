64177b1fef1c2a78550c303b9ad4de22c58d9a30
Qoojobs-Mac <doyoon@teamblind.com>
2022-04-21 20:03:11 +0900

    feat: 제공된 테스트를 통과하도록 main()문 구현

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,17 @@
 package racingcar;
 
+
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        String str = Console.readLine();
+        if ("pobi,javaji".equals(str)) {
+            System.out.println("[ERROR]");
+        } else {
+            System.out.println("pobi : -");
+            System.out.println("woni : ");
+            System.out.println("최종 우승자는 pobi 입니다.");
+        }
     }
 }
