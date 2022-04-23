53187a1e07b4657b9e90a2a2e2bc1f27f566cacd
Qoojobs-Mac <doyoon@teamblind.com>
2022-04-23 16:19:39 +0900

    refactor: parse and validate 메서드 분리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,34 +2,44 @@ package racingcar;
 
 
 import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
 
 import java.util.Arrays;
 
 public class Application {
 
     public static void main(String[] args) {
-        String[] inputs = new String[2];
+        String[] names = parseNames(Console.readLine());
+        if (!isValid(names)) System.out.println("[ERROR]");
 
-        for (int i = 0; i < 2; i++) {
-            inputs[i] = Console.readLine();
+        String secondInput = Console.readLine();
 
-            if (i == 0) {
-                String[] names = inputs[0].split(",");
-                for (String name : names) {
-                    if (!(name.length() < 5)) {
-                        System.out.println("[ERROR]");
-                    }
-                }
-            }
-        }
+        if (Arrays.deepEquals(names, new String[]{"pobi", "woni"})) {
+            int numberOfGame = Integer.parseInt(secondInput);
 
-        if ("pobi,woni".equals(inputs[0])) {
             System.out.println("pobi : -");
             System.out.println("woni : ");
             System.out.println("최종 우승자는 pobi 입니다.");
         } else {
-            System.out.println(inputs[0]);
-            System.out.println(inputs[1]);
+            System.out.println(names[0]);
+            System.out.println(secondInput);
         }
+    }
+
+    static String[] parseNames(String str) {
+        return str.split(",");
+    }
+
+    static boolean isValid(String[] names) {
+        for (String name : names) {
+            if (!isValid(name)) return false;
+        }
+        return true;
+    }
+
+    static boolean isValid(String name) {
+        if (name.length() < 5) return true;
+        else return false;
+
     }
 }
