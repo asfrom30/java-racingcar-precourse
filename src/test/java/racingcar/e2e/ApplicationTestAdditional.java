b8317b51b4d01cb16d49422bbf7f901526c406ff
Qoojobs-Mac <doyoon@teamblind.com>
2022-04-24 16:53:46 +0900

    refactor: 테스크 패키지 정리

--- src/test/java/racingcar/e2e/ApplicationTestAdditional.java
+++ src/test/java/racingcar/e2e/ApplicationTestAdditional.java
@@ -1,8 +1,9 @@
-package racingcar;
+package racingcar.e2e;
 
 import camp.nextstep.edu.missionutils.test.NsTest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import racingcar.Application;
 
 import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInRangeTest;
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
--- src/test/java/racingcar/integrations/ScoreMapTest.java
+++ src/test/java/racingcar/integrations/ScoreMapTest.java
@@ -1,4 +1,4 @@
-package integrations;
+package racingcar.integrations;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
